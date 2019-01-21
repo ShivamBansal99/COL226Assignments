6d38c92132df47ebe5b376c20f718ed2b0657b58
Shivam Bansal <32170952+ShivamBansal99@users.noreply.github.com>
2019-01-21 16:36:28 +0530

    small change in  mult

--- assn0/bigint.ml
+++ assn0/bigint.ml
@@ -12,12 +12,13 @@ let rec addList a b = match a with
 	)
 	| hdb::secb::tl -> ((hdb+hda) mod 10) :: addRev tla ((secb+ (hdb+hda)/10)::tl) 
 );;
-let inv a = match a with 
+let rec inv a = match a with 
 | [] -> []
-| hd::sec::tl -> (hd mod 10) :: (sec + hd/10) ::tl
+| hd::sec::tl -> (hd mod 10) :: inv ((sec + hd/10) ::tl)
 | hd::[] -> (hd mod 10) :: (if  hd/10=0 then [] else [hd/10])
 ;;
 let rec multList a b = match b with 
 | [] -> [0]
+| hd::[] -> inv (List.map ( fun x -> x*hd ) a)
 | hd::tl -> add (inv (List.map ( fun x -> x*hd ) a))  (0::(mult a tl));;
 
